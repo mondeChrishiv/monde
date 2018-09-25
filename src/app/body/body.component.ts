8868fe400506b803fbbea326a13e014ddf43c51e
mondeChrishiv <43520433+mondeChrishiv@users.noreply.github.com>
2018-09-25 18:25:44 -0400

    add

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { IMessages } from '../imessages'
 
 @Component({
   selector: 'app-body',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./body.component.scss'],
 })
 export class BodyComponent implements OnInit {
-  constructor() {}
+  current: IMessages
+  constructor() {
+    this.current = {
+      msg1:'We\'re so excited to see you here . . .',
+      msg2:'Welcome to the Yummilicious world of mondé'
+
+    } as IMessages
+  }
 
   ngOnInit() {}
 }
---
+++ src/app/imessages.ts
@@ -0,0 +1,4 @@
+export interface IMessages {
+    msg1: string
+    msg2: string
+}
